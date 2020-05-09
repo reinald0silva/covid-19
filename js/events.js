4f10b6879dea38c3b8a68bbc4e3e64e3b16997e7
elizot <rei.silva0101@gmail.com>
2020-05-09 19:20:11 -0300

    v1.0.3
    Resolvido Problema de responsividade em telas menores

--- js/events.js
+++ js/events.js
@@ -1,17 +1,39 @@
 $(document).ready(function() {
-    $('#button1').addClass('active');
-    $('.main').hide();
+    $('body > div.grid-container.text-shadow > div.header > div.nav-header.mt-2 > ul > li:nth-child(1) > a').addClass('active');
+    $('#main-dados1').hide();
+    $('#main-dados2').hide();
     $('#main-dados1').show();
 
-    $('body > div > div.nav-header.mt-2 > ul > li > a').click(function() {
-        $('body > div > div.nav-header.mt-2 > ul > li a').removeClass('active');
+    $('body > div > div.header > div.nav-header.mt-2 > ul > li > a').click(function() {
+        $('body > div > div.header > div.nav-header.mt-2 > ul > li > a').removeClass('active');
         $(this).addClass('active');
-        $('.main').hide();
+
+        $('#main-dados1').hide();
+        $('#main-dados2').hide();
 
         var activeTabe = $(this).attr('href');
 
-        $(activeTabe).show();
 
+        $(activeTabe).show();
         return false;
-    })
+    });
+
+
+
+    // $('#button1').addClass('active');
+    // $('.main').hide();
+    // $('#main-dados1').show();
+
+    // $('body > div > div.nav-header.mt-2 > ul > li > a').click(function() {
+    //     $('body > div > div.nav-header.mt-2 > ul > li a').removeClass('active');
+    //     $(this).addClass('active');
+    //     $('.main').hide();
+
+    //     var activeTabe = $(this).attr('href');
+    //     console.log(activeTabe)
+
+    //     $(activeTabe).show();
+
+    //     return false;
+    // })
 });
